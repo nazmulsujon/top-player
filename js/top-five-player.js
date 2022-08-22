8b312c3d0ccab3cadfb6b3280017859836b0e709
unknown <nazmul sujon>
2022-08-22 18:40:31 +0600

    top five player section added with modify

--- js/top-five-player.js
+++ js/top-five-player.js
@@ -25,42 +25,54 @@ function displayTopFivePlayer(){
     } 
 }
 
+function disable(btnId){
+    const button = document.getElementById(btnId);
+    button.disabled = true;
+}
+
 /*******add Event with neymar's button*******/
 document.getElementById('btn-neymar').addEventListener('click',function(){
-    const playerName = addFavouritePlayerByID('neymar')
+    const playerName = addFavouritePlayerByID('neymar');
     allFavPlayer.push(playerName);
     displayTopFivePlayer();
+    disable('btn-neymar');
+   
 })
 
 /*******add Event with messi's button*******/
 document.getElementById('btn-messi').addEventListener('click',function(){
-    const playerName = addFavouritePlayerByID('messi')
+    const playerName = addFavouritePlayerByID('messi');
     allFavPlayer.push(playerName);
     displayTopFivePlayer();
+    disable('btn-messi');
 })
 
 /*******add Event with marcelo's button*******/
 document.getElementById('btn-marcelo').addEventListener('click',function(){
-    const playerName = addFavouritePlayerByID('marcelo')
+    const playerName = addFavouritePlayerByID('marcelo');
     allFavPlayer.push(playerName);
     displayTopFivePlayer();
+    disable('btn-marcelo');
 })
 
 /*******add Event with mbappe's button*******/
 document.getElementById('btn-mbappe').addEventListener('click',function(){
-    const playerName = addFavouritePlayerByID('mbappe')
+    const playerName = addFavouritePlayerByID('mbappe');
     allFavPlayer.push(playerName);
     displayTopFivePlayer();
+    disable('btn-mbappe');
 })
 /*******add Event with silva's button*******/
 document.getElementById('btn-silva').addEventListener('click',function(){
-    const playerName = addFavouritePlayerByID('silva')
+    const playerName = addFavouritePlayerByID('silva');
     allFavPlayer.push(playerName);
     displayTopFivePlayer();
+    disable('btn-silva');
 })
 /*******add Event with kaka's button*******/
 document.getElementById('btn-kaka').addEventListener('click',function(){
-    const playerName = addFavouritePlayerByID('kaka')
+    const playerName = addFavouritePlayerByID('kaka');
     allFavPlayer.push(playerName);
     displayTopFivePlayer();
+    disable('btn-kaka');
 })
