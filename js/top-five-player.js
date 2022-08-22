f484371a4cac6798301d9b5a0a88e0f2cf1f07f3
unknown <nazmul sujon>
2022-08-22 15:59:41 +0600

    top five player section added

--- js/top-five-player.js
+++ js/top-five-player.js
@@ -1,21 +1,66 @@
-const allFavPlayer = [];
-
-const selectedPlayer = document.getElementById('selected-player');
-
-function displayTopFivePlayer(){
-    for(let i = 0; i <= allFavPlayer.length; i++){
-        const player = allFavPlayer[i];
-       
-    }
+/****** common function for add favourite player ******/
+function addFavouritePlayerByID(playerId){
+    const playerNameElement = document.getElementById(playerId);
+    const playerName = playerNameElement.innerText;
+    return playerName;
 }
 
+const allFavPlayer = [];
+/****** common function for display top five favourite player ******/
+function displayTopFivePlayer(){
+    const selectedPlayer = document.getElementById('selected-player');
+    selectedPlayer.innerText = '';
+    for(let i = 0; i < allFavPlayer.length; i++){
+        const player = allFavPlayer[i];
+       
+        if(allFavPlayer.length < 6){
+                const li = document.createElement('li');
+                li.innerText =player;
+                selectedPlayer.appendChild(li);
+        }  
+                   
+    } 
+    if(allFavPlayer.length > 5){
+        alert('do not select more!')  
+    } 
+}
+
+/*******add Event with neymar's button*******/
 document.getElementById('btn-neymar').addEventListener('click',function(){
     const playerName = addFavouritePlayerByID('neymar')
     allFavPlayer.push(playerName);
     displayTopFivePlayer();
 })
 
+/*******add Event with messi's button*******/
 document.getElementById('btn-messi').addEventListener('click',function(){
     const playerName = addFavouritePlayerByID('messi')
-    allFavPlayer.push(playerName);  
+    allFavPlayer.push(playerName);
+    displayTopFivePlayer();
+})
+
+/*******add Event with marcelo's button*******/
+document.getElementById('btn-marcelo').addEventListener('click',function(){
+    const playerName = addFavouritePlayerByID('marcelo')
+    allFavPlayer.push(playerName);
+    displayTopFivePlayer();
+})
+
+/*******add Event with mbappe's button*******/
+document.getElementById('btn-mbappe').addEventListener('click',function(){
+    const playerName = addFavouritePlayerByID('mbappe')
+    allFavPlayer.push(playerName);
+    displayTopFivePlayer();
+})
+/*******add Event with silva's button*******/
+document.getElementById('btn-silva').addEventListener('click',function(){
+    const playerName = addFavouritePlayerByID('silva')
+    allFavPlayer.push(playerName);
+    displayTopFivePlayer();
+})
+/*******add Event with kaka's button*******/
+document.getElementById('btn-kaka').addEventListener('click',function(){
+    const playerName = addFavouritePlayerByID('kaka')
+    allFavPlayer.push(playerName);
+    displayTopFivePlayer();
 })
--- js/utilities.js
+++
@@ -1,6 +0,0 @@
-function addFavouritePlayerByID(playerId){
-    const playerNameElement = document.getElementById(playerId);
-    const playerName = playerNameElement.innerText;
-    return playerName;
-}
-
